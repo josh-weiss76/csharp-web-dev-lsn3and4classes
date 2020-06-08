8ceb1852195595f051be4987ae2539bfe4f8bbb5
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-06-08 12:27:40 -0400

    fixed message displayed

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -10,7 +10,7 @@ namespace SchoolPractice
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
             Student newStudent = new Student("Josh", 123, 1, 4.0);
             List<Student>currentEnrollment = Course.ClassList();
-            Console.WriteLine("There are " + currentEnrollment.Count + "students in the class.");
+            Console.WriteLine("There are " + currentEnrollment.Count + " students in the class.");
             
         }
     }
